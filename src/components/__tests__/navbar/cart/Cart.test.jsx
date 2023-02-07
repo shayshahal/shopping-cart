b0ffc4b248da7df9716a8161eaf77aa8fbbc810b
shayshahal <shay840@gmail.com>
2023-02-07 19:58:54 +0200

    add Cart Component tests

--- src/components/__tests__/navbar/cart/Cart.test.jsx
+++ src/components/__tests__/navbar/cart/Cart.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen} from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import userEvent from "@testing-library/user-event";
 
 import CartToggle from '../../../navbar/cart/Cart';
