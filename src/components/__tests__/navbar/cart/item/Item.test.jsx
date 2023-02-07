6a280419440b486ca51f39fe49c75fbb4792c42f
shayshahal <shay840@gmail.com>
2023-02-07 19:58:37 +0200

    add Item Component tests

--- src/components/__tests__/navbar/cart/item/Item.test.jsx
+++ src/components/__tests__/navbar/cart/item/Item.test.jsx
@@ -1,3 +1,38 @@
-import {test, describe} from 'vitest'
+import React from 'react';
+import { render, screen} from '@testing-library/react';
 
-test('', ()=>{})
+import Item from '../../../../navbar/cart/item/Item';
+
+const name = 'blah', price = 2000;
+const item = <Item name={name} price={price}/>
+
+describe('Item component tests', ()=>{
+    test('Item renders successfully', ()=>{
+        render(item)
+
+        expect(screen.getByTestId('item')).toBeInTheDocument();
+    })
+    test('Item renders a title successfully', ()=>{
+        render(item)
+
+        expect(item).toContainElement(screen.getByRole('heading'));
+        expect(screen.getByRole('heading').textContent).toBe(price)
+    })
+    test('Price renders successfully', ()=>{
+        render(item)
+
+        expect(item).toContainElement(screen.getByText(price));
+    })
+    test('Item renders an image successfully', ()=>{
+        render(item)
+
+        expect(item).toContainElement(screen.getByRole('img'));
+    })
+
+    test('Item renders a control component', ()=>{
+        render(item)
+
+        expect(item).toContainElement(screen.getByTestId('control'));
+    })
+
+})
