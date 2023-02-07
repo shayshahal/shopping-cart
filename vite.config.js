d117925fabdd7f6c8f841c071ea5166ce2210356
shayshahal <shay840@gmail.com>
2023-02-07 16:31:38 +0200

    vitest setup

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types='vite/client'/>
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -9,8 +12,8 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'happy-dom',
+    environment: 'jsdom',
     css: true,
-    setupFiles: './src/components/__tests__/setup.js'
+    setupFiles: ['./src/components/__tests__/setup.js']
   },
 })
