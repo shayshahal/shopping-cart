2f37ddb50a4c682e4e21e46abcf1f3020ffdacb1
shayshahal <shay840@gmail.com>
2023-02-07 16:31:14 +0200

    Add CartToggle component

---
+++ src/components/navbar/cart/CartToggle.jsx
@@ -0,0 +1,9 @@
+import styles from '/src/styles/navbar/cart/CartToggle.module.css'
+
+export default function Cart() {
+    return (
+        <div className="CartToggle">
+        </div>
+    )
+}
+
