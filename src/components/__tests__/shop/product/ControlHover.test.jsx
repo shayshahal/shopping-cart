4f701b86406436767ba8620f71e4f52407eb226c
shayshahal <shay840@gmail.com>
2023-02-08 23:04:21 +0200

    finish ControlHover component tests

--- src/components/__tests__/shop/product/ControlHover.test.jsx
+++ src/components/__tests__/shop/product/ControlHover.test.jsx
@@ -1,3 +1,32 @@
-import {test, describe} from 'vitest'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
 
-test('', ()=>{})
+import ControlHover from '../../shop/product/ControlHover';
+
+const onAddMock = vi.fn();
+
+describe('ControlHover component tests', () => {
+	test('should render ControlHover component', () => {
+		render(<ControlHover onAddMock={onAddMock} />);
+		expect(screen.getByTestId('control-hover')).toBeInTheDocument();
+	});
+	test('should render ControlHover invisible', () => {
+		render(<ControlHover onAddMock={onAddMock} />);
+		expect(screen.getByTestId('control-hover')).not.toBeVisible();
+	});
+	test('should render ControlHover visible on hover', () => {
+		render(<ControlHover onAddMock={onAddMock} />);
+		userEvent.hover(screen.getByTestId('control-hover'));
+		expect(screen.getByTestId('control-hover')).toBeVisible();
+	});
+	test('should render ControlHover with two buttons', () => {
+		render(<ControlHover onAddMock={onAddMock} />);
+		expect(screen.getAllByRole('button')).toHaveLength(2);
+	});
+	test('should render ControlHover with one button', () => {
+		render(<ControlHover onAddMock={onAddMock} />);
+		userEvent.click(screen.getAllByRole('button')[1]);
+		expect(onAddMock).toHaveBeenCalled();
+	});
+});
