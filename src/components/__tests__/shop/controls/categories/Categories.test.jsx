0cee10563619ab9b855b60da7cf0f6b17ed3fb99
shayshahal <shay840@gmail.com>
2023-02-08 16:06:14 +0200

    finish Categories test

---
+++ src/components/__tests__/shop/controls/categories/Categories.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { vi } from 'vitest';
+
+import Categories from '../../../controls/categories/Categories';
+
+const categories = [
+    'category1',
+    'category2',
+    'category3',
+    'category4'
+]
+
+describe('Categories tests', () => {
+    test('renders Categories successfully', () => {
+        render(<Categories onClick={vi.fn()} categoryList={categories}/>);
+        expect(screen.getByRole('list')).toBeInTheDocument();
+    });
+    test('renders Categories items successfully', () => {
+        render(<Categories onClick={vi.fn()} categoryList={categories}/>);
+        expect(screen.getByRole('list')).toContainElement(screen.getByRole('listitem'));
+        expect(screen.getAllByRole('listitem').length).toBe(5);
+    })
+})
