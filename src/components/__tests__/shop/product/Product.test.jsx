1d9022d3d4095c1f53e82786f197e20829ed3fd2
shayshahal <shay840@gmail.com>
2023-02-08 23:11:54 +0200

    finish Product component tests

--- src/components/__tests__/shop/product/Product.test.jsx
+++ src/components/__tests__/shop/product/Product.test.jsx
@@ -1,3 +1,51 @@
-import {test, describe} from 'vitest'
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { vi } from 'vitest';
+import Product from '../../shop/product/Product';
 
-test('', ()=>{})
+const addProductMock = vi.test();
+
+describe('Product component tests', () => {
+	test('should render Product component', () => {
+		render(
+			<Product
+				addProduct={addProductMock}
+				name='watch'
+				price={3000}
+			/>
+		);
+		expect(screen.getByRole('listitem')).toBeInTheDocument();
+	});
+	test('should render name as heading', () => {
+        render(
+			<Product
+				addProduct={addProductMock}
+				name='watch'
+				price={3000}
+			/>
+		);
+
+        expect(screen.getByRole('listitem')).toContainElement(screen.getByRole('heading'));
+        expect(screen.getByRole('heading')).toHaveTextContent('watch');
+    });
+    test('should render price', () => {
+        render(
+            <Product 
+                addProduct={addProductMock}
+                name='watch'
+                price={3000}
+                />)
+
+        expect(screen.getByRole('listitem')).toContainElement(screen.getByText('3000'));
+    })
+    test('should render image', () => {
+        render(
+            <Product 
+                addProduct={addProductMock}
+                name='watch'
+                price={3000}
+                />)
+
+        expect(screen.getByRole('listitem')).toContainElement(screen.getByRole('img'));
+    })
+});
