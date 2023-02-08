90b1b08bc1ab9ee53b19ae482c444f031b34ba5a
shayshahal <shay840@gmail.com>
2023-02-08 18:43:08 +0200

    add Sort Component tests

--- src/components/__tests__/shop/controls/sorts/Sort.test.jsx
+++ src/components/__tests__/shop/controls/sorts/Sort.test.jsx
@@ -1,3 +1,28 @@
-import {test, describe} from 'vitest'
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { vi } from 'vitest';
 
-test('', ()=>{})
+import Sort from '../../../controls/sorts/Sort';
+
+const onClickMock = vi.fn();
+
+describe('Sort Component tests', () => {
+	test('Sort component renders successfully', () => {
+		render(
+			<Sort
+				onClick={onClickMock}
+				name='Price'
+			/>
+		);
+		expect(screen.getByRole('listitem')).toBeInTheDocument();
+	});
+    test('Call onChange on click', () => {
+        render(
+			<Sort
+				onClick={onClickMock}
+				name='Price'
+			/>
+		);
+        expect(onClickMock).toHaveBeenCalled();
+    })
+});
