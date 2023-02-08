0018c36f8af52b4c76c5fa19f2d1da33ba67253f
shayshahal <shay840@gmail.com>
2023-02-08 16:54:31 +0200

    add snapshot to category list tests

--- src/components/__tests__/shop/controls/categories/Categories.test.jsx
+++ src/components/__tests__/shop/controls/categories/Categories.test.jsx
@@ -4,21 +4,29 @@ import { vi } from 'vitest';
 
 import Categories from '../../../controls/categories/Categories';
 
-const categories = [
-    'category1',
-    'category2',
-    'category3',
-    'category4'
-]
+const categories = ['category1', 'category2', 'category3', 'category4'];
 
-describe('Categories tests', () => {
-    test('renders Categories successfully', () => {
-        render(<Categories onClick={vi.fn()} categoryList={categories}/>);
-        expect(screen.getByRole('list')).toBeInTheDocument();
-    });
-    test('renders Categories items successfully', () => {
-        render(<Categories onClick={vi.fn()} categoryList={categories}/>);
-        expect(screen.getByRole('list')).toContainElement(screen.getByRole('listitem'));
-        expect(screen.getAllByRole('listitem').length).toBe(5);
-    })
-})
+describe('Categories component tests', () => {
+	test('renders Categories successfully', () => {
+		render(
+			<Categories
+				onClick={vi.fn()}
+				categoryList={categories}
+			/>
+		);
+		expect(screen.getByRole('list')).toBeInTheDocument();
+	});
+	test('renders Categories items successfully', () => {
+		render(
+			<Categories
+				onClick={vi.fn()}
+				categoryList={categories}
+			/>
+		);
+		expect(screen.getByRole('list')).toContainElement(
+			screen.getByRole('listitem')
+		);
+		expect(screen.getAllByRole('listitem').length).toBe(5);
+		expect(screen.getByRole('list')).toMatchSnapshot();
+	});
+});
