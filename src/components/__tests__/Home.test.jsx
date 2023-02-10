938e184f731bf228107039c80e5ea45d958449f4
shayshahal <shay840@gmail.com>
2023-02-10 12:20:48 +0200

    home test fix

--- src/components/__tests__/Home.test.jsx
+++ src/components/__tests__/Home.test.jsx
@@ -1,21 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import Home from '../Home';
 describe('Home tests', () => {
 	test('Home should render ', () => {
-		render(<Home />);
+		render(
+			<MemoryRouter>
+				<Home />
+			</MemoryRouter>
+		);
 		expect(screen.getByRole('main')).toBeInTheDocument();
 	});
 	test('should render an h1', () => {
-		render(<Home />);
+		render(
+			<MemoryRouter>
+				<Home />
+			</MemoryRouter>
+		);
 		expect(screen.getByRole('heading')).toBeInTheDocument();
 	});
 	test('should render a paragraph', () => {
-		render(<Home />);
+		render(
+			<MemoryRouter>
+				<Home />
+			</MemoryRouter>
+		);
 		expect(screen.getByTestId('paragraph')).toBeInTheDocument();
 	});
 	test('should render a button', () => {
-		render(<Home />);
-		expect(screen.getByRole('button')).toBeInTheDocument();
+		render(
+			<MemoryRouter>
+				<Home />
+			</MemoryRouter>
+		);
+		expect(screen.getByRole('link')).toBeInTheDocument();
 	});
 });
