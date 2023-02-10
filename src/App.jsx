ef653b76f6953d45bdbfeb7d0a4577c4034f8d14
shayshahal <shay840@gmail.com>
2023-02-10 12:12:10 +0200

    add shop state and functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
-import React from "react";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./components/Home.jsx";
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './components/About.jsx';
-import Nav from './components/navbar/Nav.jsx';
-import Details from './components/Details.jsx';
 import Complete from './components/Complete.jsx';
+import Details from './components/Details.jsx';
+import Home from './components/Home.jsx';
+import Nav from './components/navbar/Nav.jsx';
 import Shop from './components/shop/Shop.jsx';
-// Do lazy loading 
+// Do lazy loading
 
 export default function App() {
-    return (
-        <BrowserRouter init>
-            <Nav/>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/Shop" element={<Shop/>} />
-                <Route path="/Shop:name" element={<Details/>} />
-                <Route path="/About" element={<About/>} />
-                <Route path="/Complete" element={<Complete/>} />
-            </Routes>
-        </BrowserRouter>
-    );
-};
+	const [cartItems, setCartItems] = useState([]);
 
+	function addItemToCart(item) {
+		setCartItems((prev) => [...prev, item]);
+	}
+
+	return (
+		<BrowserRouter init>
+			<Nav />
+			<Routes>
+				<Route
+					path='/'
+					element={<Home />}
+				/>
+				<Route
+					path='/Shop'
+					element={<Shop addProduct={addItemToCart} />}
+				/>
+				<Route
+					path='/Shop:name'
+					element={<Details />}
+				/>
+				<Route
+					path='/About'
+					element={<About />}
+				/>
+				<Route
+					path='/Complete'
+					element={<Complete />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,6 +1,56 @@
 import { useState } from 'react';
 import styles from '../../styles/Details.module.css';
+import CategoryList from './controls/categories/CategoryList';
+import FilterList from './controls/filters/FilterList';
+import SortList from './controls/sorts/SortList';
+import ProductList from './ProductList';
 
-export default function Shop() {
-	return <main className='Shop'></main>;
+export default function Shop(props) {
+	const [isDescending, setIsDescending] = useState(true);
+	const sortedAndFilteredList = useState(
+		priceSort(props.productList, isDescending)
+	);
+	function priceSort(products) {
+		return products.sort((a, b) => {
+			if (isDescending) {
+				return b.price - a.price;
+			} else {
+				return a.price - b.price;
+			}
+		});
+	}
+
+	function alphabeticalSort(products) {
+		return products.sort((a, b) => {
+			if (isDescending) {
+				return a.name.localeCompare(b.name);
+			} else {
+				return b.name.localeCompare(a.name);
+			}
+		});
+	}
+
+	function stockAmountSort(products) {
+		return products.sort((a, b) => {
+			if (isDescending) {
+				return b.stock - a.stock;
+			} else {
+				return a.stock - b.stock;
+			}
+		});
+	}
+	return (
+		<main className={styles.home}>
+			<CategoryList />
+			<FilterList />
+			<SortList
+				sortsFunctionsList={[
+					priceSort,
+					alphabeticalSort,
+					stockAmountSort,
+				]}
+			/>
+			<ProductList />
+		</main>
+	);
 }
