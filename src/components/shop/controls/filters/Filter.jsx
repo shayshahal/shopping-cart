a5b7c79be3e19829f6294765a31918ebbba4de25
shayshahal <shay840@gmail.com>
2023-02-12 17:24:20 +0200

    finish Filter and FilterList

--- src/components/shop/controls/filters/Filter.jsx
+++ src/components/shop/controls/filters/Filter.jsx
@@ -1,8 +1,21 @@
 import styles from '/src/styles/shop/controls/Filter.module.css'
+import {useEffect, useState} from 'react'
 
-export default function Filter() {
+export default function Filter(props) {
+    const [floor, setFloor] = useState('');
+	const [ceiling, setCeiling] = useState('');
+    function filterCheck(num){
+        return num >= floor && num <= ceiling;
+    }
+    useEffect(()=>{
+        props.onChange(props.name, filterCheck);
+    }, [floor, ceiling])
     return (
-        <div className="Filter">
+        <div className={styles.Filter}>
+            <label>{props.name}: </label>
+            <input className={styles.input} value={floor} type="number" onChange={e=>{setFloor(e.target.value)}} min={0}/>
+            {' - '}
+            <input className={styles.input} value={ceiling} type="number" onChange={e=>{setCeiling(e.target.value)}} min={0}/>
         </div>
     )
 }
--- src/components/shop/controls/filters/FilterList.jsx
+++ src/components/shop/controls/filters/FilterList.jsx
@@ -1,5 +1,9 @@
 import styles from '/src/styles/shop/controls/FilterList.module.css';
+import Filter from './Filter'
 
-export default function Filter() {
-	return <div className='FilterList'></div>;
+export default function FilterList(props) {
+	return <div className={styles.FilterList}>
+		<Filter name='price' onChange={props.onChange}/>
+		<Filter name='rating' onChange={props.onChange}/>
+	</div>;
 }
