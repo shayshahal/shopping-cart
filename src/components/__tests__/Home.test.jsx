120a42a4a673dd0d20f1fa55cec72473627defec
shayshahal <shay840@gmail.com>
2023-03-08 11:22:16 +0200

    update home test

--- src/components/__tests__/Home.test.jsx
+++ src/components/__tests__/Home.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import Home from '../Home';
@@ -11,28 +12,15 @@ describe('Home tests', () => {
 		);
 		expect(screen.getByRole('main')).toBeInTheDocument();
 	});
-	test('should render an h1', () => {
+
+	test.todo('button should navigate to shop', async () => {
+		const user = userEvent.setup();
 		render(
 			<MemoryRouter>
 				<Home />
 			</MemoryRouter>
 		);
-		expect(screen.getByRole('heading')).toBeInTheDocument();
-	});
-	test('should render a paragraph', () => {
-		render(
-			<MemoryRouter>
-				<Home />
-			</MemoryRouter>
-		);
-		expect(screen.getByTestId('paragraph')).toBeInTheDocument();
-	});
-	test('should render a button', () => {
-		render(
-			<MemoryRouter>
-				<Home />
-			</MemoryRouter>
-		);
-		expect(screen.getByRole('link')).toBeInTheDocument();
+
+		await user.click(screen.getByRole('button'));
 	});
 });
