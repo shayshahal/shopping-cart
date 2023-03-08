f6838d1b86e3c7550396c98bd1a675e028684c8b
shayshahal <shay840@gmail.com>
2023-03-08 11:22:44 +0200

    update shop test

--- src/components/__tests__/shop/Shop.test.jsx
+++ src/components/__tests__/shop/Shop.test.jsx
@@ -5,63 +5,13 @@ import { vi } from 'vitest';
 
 import Shop from '../../shop/Shop';
 
-const productListMock = [
-	{
-		title: 'Product 1',
-		description: 'Product 1 description',
-		price: 100,
-		stock: 100,
-		categoryId: 1,
-		id: 1,
-		images: [
-			'https://via.placeholder.com/150',
-			'https://via.placeholder.com/150',
-		],
-		thumbnail: 'https://via.placeholder.com/150',
-		discountPercentage: 50,
-		rating: 4.69,
-	},
-
-	{
-		title: 'Product 2',
-		description: 'Product 2 description',
-		price: 200,
-		stock: 100,
-		categoryId: 2,
-		id: 2,
-		images: [
-			'https://via.placeholder.com/150',
-			'https://via.placeholder.com/150',
-		],
-		thumbnail: 'https://via.placeholder.com/150',
-		discountPercentage: 50,
-		rating: 4.72,
-	},
-	{
-		title: 'Product 3',
-		description: 'Product 3 description',
-		price: 300,
-		stock: 100,
-		categoryId: 3,
-		id: 3,
-		images: [
-			'https://via.placeholder.com/150',
-			'https://via.placeholder.com/150',
-		],
-		thumbnail: 'https://via.placeholder.com/150',
-		discountPercentage: 40,
-		rating: 4.55,
-	},
-];
-const addProductMock = vi.fn();
-
 describe('Shop Component tests', () => {
 	test('render Shop successfully', () => {
 		render(
 			<MemoryRouter>
 				<Shop
-					productList={productListMock}
-					addProduct={addProductMock}
+					productList={[]}
+					addProduct={vi.fn()}
 				/>
 			</MemoryRouter>
 		);
