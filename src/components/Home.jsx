da7d53582c02bec2f4563621b4d88c37615fa524
shayshahal <shay840@gmail.com>
2023-04-18 00:17:44 +0300

    finish home restyling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,17 +3,17 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
 	return (
-		<main className={styles.Home}>
-			<span className={styles.welcome}>Welcome to</span>
-			<h1 className={styles.siteName}>Shopping Cart</h1>
+		<main className='grid content-start justify-items-center gap-8 bg-home bg-cover bg-center bg-no-repeat'>
+			<img src="/assets/shopping.png" alt="shopping image" className='max-h-64'/>
+			<h1 className='text-9xl italic font-thin text-dark-gold'>Shopping Cart</h1>
 			<span
-				className={styles.credit}
+				className='text-5xl font-thin italic'
 				data-testid='paragraph'
 			>
 				A project by Shay Shahal, following The Odin Project curriculum.
 			</span>
 			<Link
-				className={styles.shopLink}
+				className='mt-8 self-center justify-self-center border-2 border-dark-gold px-6 py-3 text-dark-gold shadow-md hover:rounded hover:bg-dark-gold hover:text-white'
 				to='/Shop'
 			>
 				Go to Shop →
