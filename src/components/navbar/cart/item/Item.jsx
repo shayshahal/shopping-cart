d9b14629b87ad01c3b6211db9fe49038abf8606a
shayshahal <shay840@gmail.com>
2023-04-18 21:51:09 +0300

    rewrite item styling with tailwind

--- src/components/navbar/cart/item/Item.jsx
+++ src/components/navbar/cart/item/Item.jsx
@@ -4,16 +4,16 @@ import styles from '/src/styles/navbar/cart/item/Item.module.css';
 export default function Item(props) {
 	return (
 		<div
-			className={styles.Item}
+			className='grid gap-x-4 p-2 pt-4 border-dark-blue border-b-2'
 			data-testid='item-container'
 		>
 			<img
 				alt={props.item.title}
 				src={props.item.thumbnail}
-				className={styles.itemImage}
+				className='row-span-2 object-contain'
 			/>
-			<h3 className={styles.itemName}>{props.item.title}</h3>
-			<p className={styles.itemPrice}>
+			<h3 className='text-end italic'>{props.item.title}</h3>
+			<p className='text-end font-medium pb-4'>
 				{props.item.price.toLocaleString('en-US')}$
 			</p>
 			<Control
