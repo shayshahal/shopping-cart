ed6843e1d7884b54c8116fdad84a3947999ad06d
shayshahal <shay840@gmail.com>
2023-04-19 15:03:48 +0300

    change onClick to onChange to fix warning

--- src/components/shop/controls/Layout.jsx
+++ src/components/shop/controls/Layout.jsx
@@ -3,19 +3,22 @@ import CircumIcon from '@klarr-agency/circum-icons-react';
 export default function Layout(props) {
 	const handleClick = (e) => props.setLayout(e.currentTarget.id);
 	return (
-		<div className='grid w-28 grid-cols-2 items-center rounded-full border-2 border-very-dark-blue'>
+		<div className='grid grid-cols-2 items-center rounded-full border-2 border-very-dark-blue md:w-36'>
 			<label
 				htmlFor='Cards'
-				className={'flex justify-center rounded-l-full cursor-pointer' + (props.layout === 'Cards' ? ' bg-very-dark-blue' : ' ')}
+				className={
+					'flex cursor-pointer justify-center rounded-l-full' +
+					(props.layout === 'Cards' ? ' bg-very-dark-blue' : ' ')
+				}
 			>
 				<CircumIcon
 					name='grid_4-1'
-					color={(props.layout === 'Cards' ? '#fff' : '#070912')}
+					color={props.layout === 'Cards' ? '#fff' : '#070912'}
 					size='36px'
 				/>
 				<input
 					className='appearance-none'
-					onClick={handleClick}
+					onChange={handleClick}
 					type='radio'
 					name='layout'
 					id='Cards'
@@ -24,16 +27,19 @@ export default function Layout(props) {
 			</label>
 			<label
 				htmlFor='Blocks'
-				className={'flex justify-center rounded-r-full cursor-pointer' + (props.layout === 'Blocks' ? ' bg-very-dark-blue' : ' ')}
+				className={
+					'flex cursor-pointer justify-center rounded-r-full' +
+					(props.layout === 'Blocks' ? ' bg-very-dark-blue' : ' ')
+				}
 			>
 				<CircumIcon
 					name='grid_2-h'
-					color={(props.layout === 'Blocks' ? '#fff' : '#070912')}
+					color={props.layout === 'Blocks' ? '#fff' : '#070912'}
 					size='36px'
 				/>
 				<input
 					className='appearance-none'
-					onClick={handleClick}
+					onChange={handleClick}
 					type='radio'
 					name='layout'
 					id='Blocks'
