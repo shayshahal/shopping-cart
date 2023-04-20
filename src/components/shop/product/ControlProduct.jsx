b64b5a68fddbeaa783b648eb89a01a16c9762d88
shayshahal <shay840@gmail.com>
2023-04-20 15:04:49 +0300

    add ControlProduct styling

--- src/components/shop/product/ControlProduct.jsx
+++ src/components/shop/product/ControlProduct.jsx
@@ -1,24 +1,24 @@
 import { Link } from 'react-router-dom';
 import styles from '/src/styles/shop/product/ControlProduct.module.css';
-
+import CircumIcons from '@klarr-agency/circum-icons-react'
 export default function ControlProduct(props) {
 	return (
 		<div
-			className={styles.ControlProduct}
+			className='flex justify-between gap-2 self-end'
 			data-testid='controlProduct-container'
 		>
 			<button
 				onClick={() => props.onAdd(props.product)}
-				className={styles.button}
+				className='flex-1 border-dark-blue border-2 hover:bg-dark-blue hover:text-white'
 			>
 				Add to Cart
 			</button>
 			<Link
-				className={styles.details}
+				className='border-dark-blue border-2'
 				to={`/shop/${props.product.title}`}
 				state={props.product}
 			>
-				ⓘ
+				<CircumIcons name='circle_info'/>
 			</Link>
 		</div>
 	);
