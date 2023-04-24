7b6acc2fafd4fe995313668fe34dc1d709831a53
shayshahal <shay840@gmail.com>
2023-04-24 11:13:35 +0300

    add tailwind details rewrite

--- src/components/details/ProductGallery.jsx
+++ src/components/details/ProductGallery.jsx
@@ -1,31 +1,31 @@
 import { useState } from 'react';
-import styles from '../../styles/details/ProductGallery.module.css';
 
 export default function ProductGallery(props) {
 	const [bigImage, setBigImage] = useState(props.images.at(-1));
 
 	return (
-		<div className={styles.ProductGallery}>
+		<div className='flex flex-col place-content-center items-center gap-32'>
 			<img
 				alt={'big image'}
 				src={bigImage}
-				className={styles.bigImage}
+				className=' h-1/2 shadow-2xl'
 			/>
-			{props.images.map((image, index) => (
-				<button
-					key={index}
-					onClick={() => {
-						setBigImage(image);
-					}}
-					className={styles.imageBtn}
-				>
-					<img
-						className={styles.image}
-						alt='image button'
-						src={image}
-					/>
-				</button>
-			))}
+			<div className='flex items-stretch gap-4 flex-wrap justify-between'>
+				{props.images.map((image, index) => (
+					<button
+						key={index}
+						onClick={() => {
+							setBigImage(image);
+						}}
+					>
+						<img
+							className='object-cover shadow-xl h-20'
+							alt='image button'
+							src={image}
+						/>
+					</button>
+				))}
+			</div>
 		</div>
 	);
 }
