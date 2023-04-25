46ee6f832adce8e32abb68f49c2fb863f5a06e72
shayshahal <shay840@gmail.com>
2023-04-25 11:45:02 +0300

    redesign nav (just icons)

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,22 +1,23 @@
+import CircumIcon from '@klarr-agency/circum-icons-react';
 import { NavLink } from 'react-router-dom';
-import '../../styles/navbar/Nav.css';
+import './activeNav.css';
 import Search from './Search';
 import CartToggle from './cart/CartToggle';
 
 export default function Nav(props) {
 	return (
-		<nav className='z-40 flex flex-col md:flex-row justify-stretch bg-very-dark-blue text-center text-2xl font-thin italic text-white shadow-2xl'>
+		<nav className='opa z-40 flex flex-col justify-stretch bg-very-dark-blue text-center text-2xl font-thin italic text-white shadow-2xl md:flex-row'>
 			<NavLink
-				className='flex-1 py-3'
+				className='flex flex-1 justify-center  py-3'
 				to='/'
 			>
-				Home
+				<CircumIcon name='home' />
 			</NavLink>
 			<NavLink
-				className='flex-1 py-3'
+				className='flex flex-1 justify-center  py-3'
 				to='/Shop'
 			>
-				Shop
+				<CircumIcon name='shop' />
 			</NavLink>
 			<Search data-testid='search' />
 			<CartToggle
