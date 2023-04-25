e996c7d28567d0636f1ca4fd34630a417b84daca
shayshahal <shay840@gmail.com>
2023-04-25 11:48:10 +0300

    change colors

--- src/components/shop/controls/categories/CategoryList.jsx
+++ src/components/shop/controls/categories/CategoryList.jsx
@@ -1,7 +1,6 @@
 import CircumIcon from '@klarr-agency/circum-icons-react';
 import { useEffect, useState } from 'react';
 import Category from './Category';
-import styles from '/src/styles/shop/controls/categories/CategoryList.module.css';
 
 export default function CategoryList(props) {
 	const [categoryList, setCategoryList] = useState([]);
@@ -23,13 +22,13 @@ export default function CategoryList(props) {
 	}
 
 	return (
-		<ul className='relative col-span-2 grid flex-1 grid-cols-3 items-center gap-4 border-y-2 border-very-dark-blue border-opacity-10 p-2 md:grid-cols-2 md:gap-0 '>
+		<ul className='relative col-span-2 grid flex-1 grid-cols-3 items-center gap-4 border-y-2 border-dark-blue border-opacity-10 p-2 md:grid-cols-2 md:gap-0 '>
 			<button
 				className='absolute bottom-0.5 right-0 transition-transform hover:rotate-180'
 				onClick={handleClick}
 				name='reset'
 			>
-				<CircumIcon name='redo' />
+				<CircumIcon name='redo' color='#12264a'/>
 			</button>
 			{categoryList.map((category, i) => (
 				<Category
