2f35fdb300a0da179e5fdf89adc3668de3bfbfe6
shayshahal <shay840@gmail.com>
2023-04-25 11:44:03 +0300

    redesign product

--- src/components/shop/product/ControlProduct.jsx
+++ src/components/shop/product/ControlProduct.jsx
@@ -1,25 +1,20 @@
 import CircumIcons from '@klarr-agency/circum-icons-react';
-import { Link } from 'react-router-dom';
 import styles from '/src/styles/shop/product/ControlProduct.module.css';
 export default function ControlProduct(props) {
 	return (
 		<div
-			className='flex justify-between gap-2 self-end p-2'
+			className='flex justify-between items-center gap-2 self-end p-2'
 			data-testid='controlProduct-container'
-		>
-						<Link
-				className='border-dark-blue transition-transform hover:scale-110'
-				to={`/shop/${props.product.title}`}
-				state={props.product}
+		>			
+								<Link
+				className='border-dark-blue '
+
 			>
-				<CircumIcons name='circle_info' color='#070912'/>
-			</Link>
-			<button
-				onClick={() => props.onAdd(props.product)}
-				className='flex-1 bg-dark-blue text-white rounded-md transition-transform hover:scale-105'
-			>
-				Add to Cart
-			</button>
+				<CircumIcons name='circle_info' color='#070912' size='36px'/>
+
+
+
+
 
 		</div>
 	);
