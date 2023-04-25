9a78f9c429041f532bd9476f8902dc1b994a280b
shayshahal <shay840@gmail.com>
2023-04-25 11:49:58 +0300

    delete non necessities

--- src/components/shop/ProductList.jsx
+++ src/components/shop/ProductList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styles from '../../styles/shop/ProductList.module.css';
-import Product from './product/Product';
+import Product from './Product';
 function ProductList(props) {
 	return (
 		<ul
