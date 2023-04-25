f4fe3c2892962d9891abf78bdad5c64492ca2e8b
shayshahal <shay840@gmail.com>
2023-04-25 14:51:33 +0300

    complete redesign

--- src/components/Complete.jsx
+++ src/components/Complete.jsx
@@ -1,34 +1,43 @@
 import { useLocation } from 'react-router-dom';
-import styles from '../styles/Complete.module.css';
 
 export default function Complete() {
 	const location = useLocation();
 	console.log(location.state);
 	return (
-		<main>
-			<h1 className='px-16 py-8 text-center text-7xl font-thin italic'>
+		<main className='overflow-hidden'>
+			<h1 className='py-8 text-center text-5xl font-thin italic md:text-7xl'>
 				Thank you for purchasing in our site!
 			</h1>
-			<div className='grid grid-cols-2 gap-8 px-8 text-end'>
+			<div className='overflow-auto p-4 mx-auto lg:w-1/2'>
 				{Object.entries(location.state).map(([k, v]) => (
 					<div
 						key={k}
-						className='grid h-72 grid-cols-2 grid-rows-block border-2 border-dark-blue pe-2 shadow-xl'
+						className='flex items-center gap-2 h-24 border-b-2 border-dark-blue border-opacity-20'
 					>
+						<div className=''>
+							x{v[1]}
+						</div>
 						<img
 							alt={v[0].title}
 							src={v[0].thumbnail}
-							className='row-span-4 h-full w-full object-contain'
+							className='h-3/4 w-48 object-contain'
 						/>
-						<h3 className='pt-2 text-3xl italic'>{v[0].title}</h3>
-						<div className='text-4xl'>
+						<h3 className='pt-2 text-3xl italic font-thin'>{v[0].title}</h3>
+						<div className='text-4xl ms-auto '>
 							{v[0].price.toLocaleString('en-US')}$
-						</div>
-						<div className='row-span-2 self-end pb-2'>
-							Amount: {v[1]}
 						</div>
 					</div>
 				))}
+				<h2 className='pt-4 text-end text-2xl font-thin md:text-3xl'>
+					Total cost:{' '}
+					<span className='font-normal text-dark-blue'>
+						{Object.values(location.state).reduce(
+							(sum, v) => sum + v[0].price,
+							0
+						)}{' '}
+						$
+					</span>
+				</h2>
 			</div>
 		</main>
 	);
